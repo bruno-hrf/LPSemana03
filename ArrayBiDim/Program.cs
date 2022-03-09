06a37bba436755c8cf5c8354c222166814db04af
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-03-09 21:43:53 +0000

    resolvi conversoes no codigo

--- ArrayBiDim/Program.cs
+++ ArrayBiDim/Program.cs
@@ -7,10 +7,12 @@ namespace ArrayBiDim
         static async void Main(string[] args)
         {
             Console.WriteLine("Escreva a dimensão horizontal de um array bidimensional de floats: ");
-            int linhas = Console.ReadLine();
+            string linhas = Console.ReadLine();
+            int linhasInt = int.Parse(linhas);
 
             Console.WriteLine("Escreva a dimensão vertical do array: ");
-            int colunas = Console.ReadLine();
+            string colunas = Console.ReadLine();
+            int colunasInt = int.Parse(colunas);
 
             float [ , ] matrix = new float [linhas, colunas];
 
@@ -19,8 +21,9 @@ namespace ArrayBiDim
                 for(int j=0 ; j<matrix.GetLength(1); i++)
                 {
                     Console.WriteLine($"Qual o número para a posição [{i},{j}]");
-                    float valor = Console.ReadLine();
-                    matrix [i,j] = valor;
+                    string valor = Console.ReadLine();
+                    float valorFloat = float.Parse(valor);
+                    matrix[i,j]  = valorFloat;
                 }
             }
 
