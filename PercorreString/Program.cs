08cbf48d4966664c4725a2426dff31df9718f07f
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-03-09 21:22:26 +0000

    Exercicio feito

---
+++ PercorreString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            string c;
+
+            Console.WriteLine("Escreva uma frase: ");
+            s = Console.ReadLine();
+
+            Console.WriteLine("Escreva um character: ");
+            c = Console.ReadLine();
+            int charConvert = Convert.ToInt32(c[0]);
+
+            for(int i = 0 ; i<s.Length ; i++ )
+            {
+                int fraseConvert = Convert.ToInt32(s[i]);
+                
+                if (fraseConvert != charConvert)
+                {
+                    Console.Write(s[i]);
+                }
+            }
+
+
+        }
+    }
+}
