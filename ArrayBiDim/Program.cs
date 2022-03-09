19dd89e57825c1392203de08ac764b5e9c878d65
Bruno Rodrigues <rodrigues_bruno@outlook.pt>
2022-03-09 21:38:22 +0000

    matrix feita

---
+++ ArrayBiDim/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrayBiDim
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a dimensão horizontal de um array bidimensional de floats: ");
+            int linhas = Console.ReadLine();
+
+            Console.WriteLine("Escreva a dimensão vertical do array: ");
+            int colunas = Console.ReadLine();
+
+            float [ , ] matrix = new float [linhas, colunas];
+
+            for(int i=0 ; i<matrix.GetLength(0); i++)
+            {
+                for(int j=0 ; j<matrix.GetLength(1); i++)
+                {
+                    Console.WriteLine($"Qual o número para a posição [{i},{j}]");
+                    float valor = Console.ReadLine();
+                    matrix [i,j] = valor;
+                }
+            }
+
+        }
+    }
+}
