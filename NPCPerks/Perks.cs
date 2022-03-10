df2e1943ef5a52513951b348fc336836b72c1e70
Bruno Fernandes <a22103346@alunos.ulht.pt>
2022-03-10 14:58:30 +0000

    Correção de erros

--- NPCPerks/Perks.cs
+++ NPCPerks/Perks.cs
@@ -1,9 +1,13 @@
 using System;
 
-enum Perks
-{
-    Stealth = 1 << 0, // 1
-    Combat = 1 << 1, // 2
-    Lockpick = 1 << 2, // 4
-    Luck = 1 << 3 // 8
-};
+
+namespace NPCPerks
+{   [Flags]
+    enum Perks
+    {
+        Stealth = 1 << 0, // 1
+        Combat = 1 << 1, // 2
+        Lockpick = 1 << 2, // 4
+        Luck = 1 << 3 // 8
+    };
+}
--- NPCPerks/Program.cs
+++ NPCPerks/Program.cs
@@ -10,10 +10,76 @@ namespace NPCPerks
             string numeroNPCstring;
 
             Console.WriteLine("Quantos NPCs existem: ");
-            Console.ReadLine(numeroNPC);
+            numeroNPCstring = Console.ReadLine();
 
             int numeroNPCint = int.Parse(numeroNPCstring);
 
+
+            Perks[] matrix = new Perks[numeroNPCint];
+
+            for(int i=0; i< matrix.Length ; i++)
+            {
+
+                string resposta1 , resposta2 , resposta3, resposta4;
+                
+                
+
+                
+                Console.WriteLine("O NPC é class Stealth? (s / n): ");
+                Console.ReadLine(resposta1);
+
+                int resposta1int = int.Parse(resposta1);
+
+                if(resposta1int == "s")
+                {
+                    matrix[i] |= Perks.Stealth;
+
+                }
+
+                Console.WriteLine("O NPC é class Combat? (s / n): ");
+                Console.ReadLine(resposta2);
+
+                int resposta2int = int.Parse(resposta2);
+
+                if(resposta2int == "s")
+                {
+                    matrix[i] |= Perks.Combat;
+
+                }
+
+
+                Console.WriteLine("O NPC é class Lockpick? (s / n): ");
+                Console.ReadLine(resposta3);
+
+                int resposta3int = int.Parse(resposta3);
+
+                if(resposta3int == "s")
+                {
+                    matrix[i] |= Perks.LockPick;
+
+                }
+
+
+                Console.WriteLine("O NPC é class Luck?(s / n): ");
+                Console.ReadLine(resposta4);
+
+                int resposta4int = int.Parse(resposta4);
+
+                if(resposta4int == "s")
+                {
+                    matrix[i] |= Perks.Luck;
+
+                }
+
+                
+            
+
+            
+            }
+              
+            
+
+              
 
         }
     }
